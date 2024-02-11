91735bd3e3c64e5b180deb3cf999a7c3b9e59041
Masha Leonova <goryaeva.ma@gmail.com>
2024-02-11 18:09:23 +0300

    add tests ex17

--- tests/test_user_edit.py
+++ tests/test_user_edit.py
@@ -1,8 +1,15 @@
+import string
+
 from lib.my_requests import MyRequests
 from lib.base_case import BaseCase
 from lib.assertions import Assertions
+import allure
+import time
+import random
 
+@allure.epic("Changing user data cases")
 class TestUserEdit(BaseCase):
+    @allure.description("This test checks changing firstName just created user")
     def test_edit_just_created_user(self):
         #REGISTER
         register_data = self.prepare_registration_data()
@@ -49,5 +56,226 @@ class TestUserEdit(BaseCase):
             response4,
             "firstName",
             new_name,
+            "Wrong name of user after edit"
+        )
+
+    @allure.description("This test checks changing firstName unauthorized user")
+    def test_edit_unauthorized_user(self):
+        #REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        firstName = register_data["firstName"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        #EDIT
+        new_name = "Changed Name"
+
+        response2 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={},
+            cookies={},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response2, 400)
+
+        # LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response3 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response3, "auth_sid")
+        token = self.get_header(response3, "x-csrf-token")
+
+        # GET
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_value_by_name(
+            response4,
+            "firstName",
+            firstName,
+            "Wrong name of user after edit"
+        )
+
+    @allure.description("This test checks changing firstName of another user")
+    def test_edit_another_user(self):
+        #REGISTER 1
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email1 = register_data["email"]
+
+        time.sleep(5)
+
+        # REGISTER 2
+        register_data = self.prepare_registration_data()
+        response2 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response2, 200)
+        Assertions.assert_json_has_key(response2, "id")
+
+        email2 = register_data["email"]
+        firstName = register_data["firstName"]
+        password = register_data["password"]
+        user_id2 = self.get_json_value(response2, "id")
+
+        #LOGIN1
+        login_data = {
+            'email': email1,
+            'password': password
+        }
+        response3 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response3, "auth_sid")
+        token = self.get_header(response3, "x-csrf-token")
+
+        #EDIT
+        new_name = "Changed Name"
+
+        response4 = MyRequests.put(
+            f"/user/{user_id2}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response4, 200)
+
+        # LOGIN2
+        login_data = {
+            'email': email2,
+            'password': password
+        }
+        response5 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response5, "auth_sid")
+        token = self.get_header(response5, "x-csrf-token")
+
+        #GET
+        response4 = MyRequests.get(
+            f"/user/{user_id2}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_value_by_name(
+            response4,
+            "firstName",
+            firstName,
+            "Wrong name of user after edit"
+        )
+
+    @allure.description("This test checks changing incorrect email")
+    def test_edit_incorrect_email_user(self):
+        #REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        firstName = register_data["firstName"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        #LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        #EDIT
+        new_email = "abcdeexample.com"
+
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"email": new_email}
+        )
+
+        Assertions.assert_code_status(response3, 400)
+
+        #GET
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_value_by_name(
+            response4,
+            "email",
+            email,
+            "Wrong email after edit"
+        )
+
+    @allure.description("This test checks changing short firstName")
+    def test_edit_short_name_user(self):
+        #REGISTER
+        register_data = self.prepare_registration_data()
+        response1 = MyRequests.post("/user/", data=register_data)
+
+        Assertions.assert_code_status(response1, 200)
+        Assertions.assert_json_has_key(response1, "id")
+
+        email = register_data["email"]
+        firstName = register_data["firstName"]
+        password = register_data["password"]
+        user_id = self.get_json_value(response1, "id")
+
+        #LOGIN
+        login_data = {
+            'email': email,
+            'password': password
+        }
+        response2 = MyRequests.post("/user/login", data=login_data)
+
+        auth_sid = self.get_cookie(response2, "auth_sid")
+        token = self.get_header(response2, "x-csrf-token")
+
+        #EDIT
+        new_name = random.choice(string.ascii_letters)
+
+        response3 = MyRequests.put(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid},
+            data={"firstName": new_name}
+        )
+
+        Assertions.assert_code_status(response3, 400)
+
+        #GET
+        response4 = MyRequests.get(
+            f"/user/{user_id}",
+            headers={"x-csrf-token": token},
+            cookies={"auth_sid": auth_sid}
+        )
+
+        Assertions.assert_json_value_by_name(
+            response4,
+            "firstName",
+            firstName,
             "Wrong name of user after edit"
         )
